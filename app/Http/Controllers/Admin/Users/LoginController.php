ebbecbae04f463cf1abc296eae7f37aa657cfca6
Nguyễn Trần Phú <phunguyen4869@gmail.com>
2021-12-21 17:34:29 +0700

    first commit

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('admin.users.login', [
+            'title' => 'Login',
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            //check email and password is valid
+            'email' => 'required|email:filter',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ], $request->remember)) {
+            return redirect()->route('admin.dashboard');
+        } else {
+            return redirect()->back()->withInput()->withErrors([
+                'email' => 'Email or password is invalid',
+            ]);
+        }
+    }
+}
