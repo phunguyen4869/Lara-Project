d66c43fadee0c7ed1dace2dc3d2ca1db0ed2a2f5
Nguyễn Trần Phú <phunguyen4869@gmail.com>
2021-12-23 11:27:23 +0700

    add menu form and add ckeditor to edit content

--- resources/views/admin/menu/create.blade.php
+++ resources/views/admin/menu/create.blade.php
@@ -1,5 +1,69 @@
 @extends('admin.main')
 
+@section('head')
+    <script src="/ckeditor/ckeditor.js"></script>
+@endsection
+
 @section('content')
 
+    <div class="card-header">
+        <h3 class="card-title">{{ $title }}</h3>
+    </div>
+    <!-- /.card-header -->
+    <!-- form start -->
+    <form action="#" method="POST">
+        <div class="card-body">
+            <div class="form-group">
+                <label for="menu">Tên danh mục</label>
+                <input type="text" class="form-control" id="menu" placeholder="Enter name">
+            </div>
+
+            <div class="form-group">
+                <label for="parent_id">Danh mục</label>
+                <select name="parent_id" id="parent_id" class="form-control">
+                    <option value="0">Danh mục cha</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="description">Mô tả</label>
+                <textarea name="description" id="description" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="content">Mô tả chi tiết</label>
+                <textarea name="content" id="content" class="form-control"></textarea>
+            </div>
+
+
+            <div class="form-group">
+                <label>Kích hoạt</label>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" value="1" type="radio" id="active" name="customRadio">
+                    <label for="active" class="custom-control-label">Có</label>
+                </div>
+
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" value="0" type="radio" id="not_active" name="customRadio"
+                        checked="">
+                    <label for="not_active" class="custom-control-label">Không</label>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.card-body -->
+
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Tạo danh mục</button>
+        </div>
+    </form>
+
+@endsection
+
+@section('footer')
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor 4
+        // instance, using default configuration.
+        CKEDITOR.replace('content');
+    </script>
 @endsection
